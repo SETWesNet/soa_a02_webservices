21abbd602c32bd3affa422c5fc3a8a56b60a86b9
Colin J. Mills <colin.j.mills96@gmail.com>
2017-09-20 21:40:33 -0400

    Add library manager and Json.NET package

---
+++ WebServiceInterface/WebServiceInterface/Library/LibraryManager.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WebServiceInterface.Library
+{
+    class Parameter
+    {
+        public string type { get; set; }
+        public string name { get; set; }
+    }
+      
+    class Method
+    {
+        public string name { get; set; }
+        public Parameter[] parameters { get; set; }
+    }
+
+    class ServiceInfo
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public Method[] methods { get; set; }
+    }
+
+
+    class LibraryManager
+    {
+        private string _rawContent;
+        private ServiceInfo[] allInfos;
+
+        public LibraryManager(string fileName)
+        {
+            _rawContent = File.ReadAllText(fileName);
+            allInfos = JsonConvert.DeserializeObject<ServiceInfo[]>(_rawContent);
+        }
+
+        public ServiceInfo getService(string name)
+        {
+            ServiceInfo result = null;
+
+            foreach(ServiceInfo current in allInfos)
+            {
+                if (current.name == name)
+                {
+                    result = current;
+                    break;
+                } 
+            }
+
+            return result;
+        }
+    }
+}
--- WebServiceInterface/WebServiceInterface/MainForm.cs
+++ WebServiceInterface/WebServiceInterface/MainForm.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,14 +12,24 @@ using System.Windows.Forms;
 using System.Net;
 using System.Xml;
 using System.IO;
+using WebServiceInterface.Library;
 
 namespace WebServiceInterface
 {
     public partial class MainForm : Form
     {
+        private LibraryManager configLibrary;
+        private string configPath;
+
         public MainForm()
         {
             InitializeComponent();
+            configPath = "test.txt";
+            configLibrary = new LibraryManager(configPath);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
         }
     }
 }
